e701870042211b58e448ce5a204cf7cfc3815a8b
Muhammad Waleed <waleednaveed962@gmail.com>
2024-08-14 12:42:29 +0500

    ds

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/server')
+def server():
+    return render_template('index.html')
